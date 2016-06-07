e7cf4a9ef533a6322a7de58837e27afb8ef92f6d
Gareth Harcombe-Minson <gharcombe-minson@gravie.com>
2016-06-07 15:39:28 -0500

    first go at lambda version of updater

---
+++ lambda-handler.py
@@ -0,0 +1,147 @@
+
+import urlparse
+import logging
+import sys
+import tempfile
+import yum
+import createrepo
+import boto
+import os
+import shutil
+
+# Logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Hack for creating s3 urls
+urlparse.uses_relative.append('s3')
+urlparse.uses_netloc.append('s3')
+
+# Comment this out when uploading to AWS
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
+
+class LoggerCallback(object):
+    def errorlog(self, message):
+        logging.error(message)
+
+    def log(self, message):
+        message = message.strip()
+        if message:
+            logging.info(message)
+
+class S3Grabber(object):
+    def __init__(self, baseurl):
+        base = urlparse.urlsplit(baseurl)
+        self.baseurl = baseurl
+        self.basepath = base.path.lstrip('/')
+        self.bucket = boto.connect_s3().get_bucket(base.netloc)
+
+    def _getkey(self, url):
+        if url.startswith(self.baseurl):
+            url = url[len(self.baseurl):].lstrip('/')
+        key = self.bucket.get_key(os.path.join(self.basepath, url))
+        if not key:
+            raise createrepo.grabber.URLGrabError(14, '%s not found' % url)
+        return key
+
+    def urlgrab(self, url, filename, **kwargs):
+        key = self._getkey(url)
+        logging.info('downloading: %s', key.name)
+        key.get_contents_to_filename(filename)
+        return filename
+
+    def urldelete(self, url):
+        key = self._getkey(url)
+        logging.info('removing: %s', key.name)
+        key.delete()
+
+    def syncdir(self, dir, url):
+        """Copy all files in dir to url, removing any existing keys."""
+        base = os.path.join(self.basepath, url)
+        existing_keys = list(self.bucket.list(base))
+        new_keys = []
+        for filename in sorted(os.listdir(dir)):
+            key = self.bucket.new_key(os.path.join(base, filename))
+            key.set_contents_from_filename(os.path.join(dir, filename))
+            new_keys.append(key.name)
+            logging.info('uploading: %s', key.name)
+        for key in existing_keys:
+            if key.name not in new_keys:
+                logging.info('removing: %s', key.name)
+                key.delete()
+
+
+def update_repodata(bucketName, key, operation):
+    fileName = key[key.rfind("/")+1:]
+    repoPath = key[:key.rfind("/")]
+
+    logger.info("fileName={0}".format(fileName))
+    logger.info("repoPath={0}".format(repoPath))
+
+    tmpdir = tempfile.mkdtemp()
+    s3base = urlparse.urlunsplit(("s3", bucketName, repoPath, "", ""))
+    s3grabber = S3Grabber(s3base)
+
+    # Set up temporary repo that will fetch repodata from s3
+    yumbase = yum.YumBase()
+    yumbase.preconf.disabled_plugins = '*'
+    yumbase.conf.cachedir = os.path.join(tmpdir, 'cache')
+    yumbase.repos.disableRepo('*')
+    repo = yumbase.add_enable_repo('s3')
+    repo._grab = s3grabber
+    repo._urls = [os.path.join(s3base, '')]
+    # Ensure that missing base path doesn't cause trouble
+    repo._sack = yum.sqlitesack.YumSqlitePackageSack(
+        createrepo.readMetadata.CreaterepoPkgOld)
+
+    # Create metadata generator
+    mdconf = createrepo.MetaDataConfig()
+    mdconf.directory = tmpdir
+    mdconf.pkglist = yum.packageSack.MetaSack()
+    mdgen = createrepo.MetaDataGenerator(mdconf, LoggerCallback())
+    mdgen.tempdir = tmpdir
+    mdgen._grabber = s3grabber
+
+    if operation == "add":
+        # Combine existing package sack with new rpm file list
+        new_packages = yum.packageSack.PackageSack()
+        newpkg = mdgen.read_in_package(os.path.join(s3base, fileName))
+        newpkg._baseurl = ''   # don't leave s3 base urls in primary metadata
+        new_packages.addPackage(newpkg)
+    else:
+        # Remove deleted package
+        logger.info("Delete package {0}".format(key))
+        yumbase.pkgSack.delPackage()
+
+
+
+    mdconf.pkglist.addSack('existing', yumbase.pkgSack)
+    mdconf.pkglist.addSack('new', new_packages)
+
+    # Write out new metadata to tmpdir
+    mdgen.doPkgMetadata()
+    mdgen.doRepoMetadata()
+    mdgen.doFinalMove()
+
+    # Replace metadata on s3
+    s3grabber.syncdir(os.path.join(tmpdir, 'repodata'), 'repodata')
+
+    shutil.rmtree(tmpdir)
+
+
+def handle(event, context):
+    record = event['Records'][0]
+    eventType = record['eventName']
+    s3Elem = record['s3']
+    bucketName = s3Elem['bucket']['name']
+    key = s3Elem['object']['key']
+
+    logger.info("Got Event {0}:{1}/{2}".format(eventType, bucketName, key))
+
+    if "Created" in eventType:
+        update_repodata(bucketName, key, "add")
+    elif "Removed" in eventType and "Marker" not in eventType:
+        update_repodata(bucketName, key, "remove")
+    else:
+        logger.error("Ignoring EventType {0}".format(eventType))
