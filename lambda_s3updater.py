8a663b4474f115e9ec8845da6c5a60fbb07abd0b
Seth <402885+Setheck@users.noreply.github.com>
2018-10-18 17:22:32 -0700

    Lambda updates (#3)
    * allow setting loglevel via environment
    
    * fix makefile...
    
    * add support for relative pathing under the repo directory

--- lambda_s3updater.py
+++ lambda_s3updater.py
@@ -1,7 +1,7 @@
 
 import urlparse
 import logging
-import sys
+# import sys  # For StreamHandler
 import tempfile
 import createrepo
 import yum
@@ -12,14 +12,15 @@ from rpmUtils.miscutils import splitFilename
 
 # Logger
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+level = os.getenv('LOG_LEVEL', 'INFO')
+logger.setLevel(level)
 
 # Hack for creating s3 urls
 urlparse.uses_relative.append('s3')
 urlparse.uses_netloc.append('s3')
 
-sh = logging.StreamHandler(sys.stdout)
-logger.addHandler(sh)
+# sh = logging.StreamHandler(sys.stdout)
+# logger.addHandler(sh)
 
 class LoggerCallback(object):
     def errorlog(self, message):
@@ -31,11 +32,12 @@ class LoggerCallback(object):
             logging.info(message)
 
 class S3Grabber(object):
-    def __init__(self, baseurl):
+    def __init__(self, baseurl, overrides={}):
         base = urlparse.urlsplit(baseurl)
         self.baseurl = baseurl
         self.basepath = base.path.lstrip('/')
         self.bucket = boto.connect_s3().get_bucket(base.netloc)
+        self.fileNameOverrides = overrides
 
     def _getkey(self, url):
         if url.startswith(self.baseurl):
@@ -47,7 +49,8 @@ class S3Grabber(object):
 
     def urlgrab(self, url, filename, **kwargs):
         key = self._getkey(url)
-        logging.debug('downloading: %s', key.name)
+        filename = self.fileNameOverrides.get(filename, filename)
+        logging.debug('downloading: %s to %s', key.name, filename)
         key.get_contents_to_filename(filename)
         return filename
 
@@ -73,22 +76,33 @@ class S3Grabber(object):
 
 
 def update_repodata(bucketName, key, operation):
+    logging.debug('Key: %s', key)
     if key.rfind("/") > -1:
       fileName = key[key.rfind("/")+1:]
-      repoPath = key[:key.rfind("/")]
+      p = key.partition('/')
+      repoPath = p[0]  # repoPath is always under first dir.
+      relativeFileName = p[2]
+      packagePath = relativeFileName[:relativeFileName.rfind("/")]
     else:
       fileName = key
+      relativeFileName = fileName
       repoPath = ""
+      packagePath = ''
 
     (name, version, release, epoch, arch) = splitFilename(fileName)
 
     logger.debug("fileName={0}".format(fileName))
+    logger.debug("relativeFileName={0}".format(relativeFileName))
+    logger.debug("packagePath={0}".format(packagePath))
     logger.debug("repoPath={0}".format(repoPath))
 
     tmpdir = tempfile.mkdtemp()
+    os.makedirs(os.path.join(tmpdir, packagePath))
 
     s3base = urlparse.urlunsplit(("s3", bucketName, repoPath, "", ""))
-    s3grabber = S3Grabber(s3base)
+    overridekey = os.path.join(tmpdir, fileName)
+    overrideval = os.path.join(tmpdir, relativeFileName)
+    s3grabber = S3Grabber(s3base, {overridekey: overrideval})
 
     # Set up temporary repo that will fetch repodata from s3
     yumbase = yum.YumBase()
@@ -113,8 +127,8 @@ def update_repodata(bucketName, key, operation):
     new_packages = yum.packageSack.PackageSack()
     if operation == "add":
         # Combine existing package sack with new rpm file list
-        newpkg = mdgen.read_in_package(os.path.join(s3base, fileName))
-        newpkg._baseurl = ''   # don't leave s3 base urls in primary metadata
+        newpkg = mdgen.read_in_package(os.path.join(s3base, relativeFileName))
+        newpkg._baseurl = ''  # don't leave s3 base urls in primary metadata
         new_packages.addPackage(newpkg)
     else:
         # Remove deleted package
